f8124ef9cd2ae5ebc18f2d364735d2bed49f9c9c
ghostbrine_ <ghostbrine@gmx.de>
2014-10-17 17:48:44 +0200

    AutoPush

--- git.sh
+++ git.sh
@@ -3,6 +3,6 @@ do
   echo 'IchTuNixÄnderung ' >> changed.txt
   git add -A *
   git commit -a --message=AutoPush --author="ghostbrine_ <ghostbrine@gmx.de>"
-  push -u test master
+  git push -u test master
   sleep 30
 done
