5254669d6e4eca8fada54de2bdd682921f30e3f0
ghostbrine_ <ghostbrine@gmx.de>
2014-10-18 11:00:15 +0200

    AutoPush

--- src/me/ghostbrine/devathlon/Core.java
+++ src/me/ghostbrine/devathlon/Core.java
@@ -1,5 +1,8 @@
 package me.ghostbrine.devathlon;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
@@ -11,14 +14,20 @@ public class Core extends JavaPlugin {
     }
 
     private EffectsManager effectsManager;
+    private ProtocolManager protocolManager;
 
     @Override
     public void onLoad() {
         instance = this;
+        protocolManager = ProtocolLibrary.getProtocolManager();
     }
 
     @Override
     public void onEnable() {
         effectsManager = new EffectsManager();
+    }
+
+    public void spawnParticleAt(int id, Location loc) {
+        
     }
 }
