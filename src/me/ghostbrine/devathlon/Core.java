db042f8c8f08b0b65543d7a81769d4b4601ca2bc
ghostbrine_ <ghostbrine@gmx.de>
2014-10-18 11:10:18 +0200

    AutoPush

--- src/me/ghostbrine/devathlon/Core.java
+++ src/me/ghostbrine/devathlon/Core.java
@@ -1,7 +1,9 @@
 package me.ghostbrine.devathlon;
 
+import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
 import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -27,7 +29,20 @@ public class Core extends JavaPlugin {
         effectsManager = new EffectsManager();
     }
 
-    public void spawnParticleAt(int id, Location loc) {
-        
+    public void spawnParticleAt(int id, Location loc, int count, float data) {
+        PacketContainer packet = protocolManager.createPacket(PacketType.Play.Server.WORLD_PARTICLES);
+        packet.getIntegers()
+                .write(0, id)
+                .write(1, count);
+        packet.getBooleans()
+                .write(0, true);
+        packet.getFloat()
+                .write(0, (float) loc.getX())
+                .write(1, (float) loc.getY())
+                .write(2, (float) loc.getZ())
+                .write(3, 0F)
+                .write(4, 0F)
+                .write(5, 0F)
+                .write(7, data);
     }
 }
